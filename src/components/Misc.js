a26e65f937b7dd0bbde1da2b9ef774bd2e283d6c
Victoria Garcia <v.elisabeth.garcia@gmail.com>
2018-06-20 13:54:52 -0700

    No really. Removed VideoStore and Misc.

--- src/components/Misc.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import SelectedCustomer from './SelectedCustomer';
-
-
-export const CustomerSpot = () => {
-  return  (
-    < SelectedCustomer /> 
-  )};
